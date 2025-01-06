8eb04547c7ab9cfeb743e2c977fa2f58fbb091c3
Igor <igor.drapak@powwowtechnologies.com>
2025-01-06 18:45:12 +0200

    add stucture for

---
+++ backend/middleware/index.mjs
@@ -0,0 +1,37 @@
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import { fileURLToPath } from 'url'
+import loggerConfig from '../config/logger.mjs'
+import cors from 'cors'
+
+// Визначення поточного файлу і директорії
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const middleware = (app) => {
+    // Middleware для підтримки CORS (Cross-Origin Resource Sharing)
+    app.use(cors())
+
+
+    // Middleware для логування запитів
+    app.use(loggerConfig)
+
+    // Middleware для парсингу JSON запитів
+    app.use(express.json())
+
+    // Middleware для парсингу URL-кодованих даних
+    app.use(express.urlencoded({ extended: false }))
+
+    // Middleware для парсингу cookies
+    app.use(cookieParser())
+
+    // Middleware для обробки статичних файлів з директорії public
+    app.use(express.static(path.join(__dirname, '../public')))
+
+    // Middleware для обробки статичних файлів з директорії uploads
+    app.use(express.static(path.join(__dirname, '../uploads')))
+
+}
+
+export default middleware
